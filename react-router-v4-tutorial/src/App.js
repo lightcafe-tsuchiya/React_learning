468a58b0f5ce2a8f24dbf2a6a6ba3a1ca3ae485c
toratora <torafusu@gmail.com>
2019-09-07 06:34:07 +0900

    modify

--- react-router-v4-tutorial/src/App.js
+++ react-router-v4-tutorial/src/App.js
@@ -67,39 +67,40 @@ const Friends = () => (
 
 const FriendList = () => (
     <div>
-        {FRIENDS.map(friend => (
-            <li key={friend.id}>
-                <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
-            </li>
-        ))}
+      {FRIENDS.map(friend => (
+        <li key={friend.id}>
+          <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+        </li>
+      ))}
     </div>
-)
-
-const Friend = props => {
-    const { id } = props.math.params;
-    const friend = friendById(id);
-
-    if(typeof friend === 'undefined'){
-        return(
-            <div>
-                <p>Friends with id '{id}' does not exist.</p>
-            </div>
-        )
-    }
-
-    const containerStyle = { border: '1px gray solid', display: 'inline-block', padding: 10};
-    const contentStyle = { margin: 0 };
-
-    return (
+  )
+  
+  const Friend = props => {
+    const { id } = props.match.params
+    const friend = friendById(id)
+  
+    if (typeof friend === 'undefined')  {
+      return (
         <div>
-            <div style={containerStyle}>
-                <p style={contentStyle}>{friend.family}></p>
-                <h1 style={contentStyle}>{friend.nameJa}></h1>
-                <p style={contentStyle}>{friend.nameEn}></p>
-            </div>
+          <p>Friends with id '{id}' does not exist.</p>
         </div>
-    );
-}
+      )
+    }
+  
+  
+    const containerStyle = { border: '1px gray solid', display: 'inline-block', padding: 10 }
+    const contentsStyle = { margin: 0 }
+  
+    return (
+      <div>
+        <div style={containerStyle}>
+          <p style={contentsStyle}>{friend.family}</p>
+          <h1 style={contentsStyle}>{friend.nameJa}</h1>
+          <p style={contentsStyle}>{friend.nameEn}</p>
+        </div>
+      </div>
+    )
+  }
 
 
 export default App;
